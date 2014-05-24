8eb801625cd7dbeb3e7c87d5bf7eb40146ab57ec
scotidler@hotmail.com <saidler@localhost.localdomain>
2014-05-24 00:10:43 -0700

    made numerous changes to fix issues when compiling on linux; more to go... -sai

--- src/sota.cpp
+++ src/sota.cpp
@@ -6,40 +6,35 @@
 #include "lexer.h"
 #include "token.h"
 #include "stream.hpp"
-#include "ast.h"
 #include <array>
 
+using namespace std;
 using namespace sota;
-
-vector<char> load(string filename) {
-    vector<char> chars;
-    ifstream file(filename, ios::in | ios::binary | ios::ate);
-    if (!file.eof() && !file.fail()) {
-        file.seekg(0, ios_base::end);
-        auto size = (unsigned int)file.tellg();
-        chars.resize(size);
-        file.seekg(0, ios_base::beg);
-        file.read(&chars[0], size);
-    }
-    return chars;
-}
+using namespace sota::lexer;
+using namespace sota::stream;
+using namespace sota::utils;
 
 int main(int argc, char* argv[])
 {
-    auto chars = load("example1.sota");
-    auto lexer = SotaLexer(chars);
+    string filename("example1.sota");
+    auto lexer = SotaLexer(filename);
+
+    Token t = { TokenType::Add, 1, 1 };
+    TokenType tt = t;
+
+    if (t == TokenType::Add)
+        cout << "works" << endl;
 
     vector<string> values;
-    vector<string> pretties;
-    vector<Token> tokens;
-    while (auto token = lexer.Scan()) {
-
-        cout << lexer.Pretty(token);
-        if (TokenType::EndOfLine == token.Type)
+    Token token;
+    do {
+        token = lexer.Scan();
+        values.push_back(token.Pretty());
+        cout << token.Pretty() << " ";
+        if (token.Type == TokenType::EndOfLine)
             cout << endl;
-        else
-            cout << " ";
-    }
+    } while (token);
+
     return 0;
 }
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,42 +2,44 @@
 #include <algorithm>
 
 namespace sota {
-    vector<string> get_lines(string filename) {
-        vector<string> lines;
-        ifstream file(filename);
-        string line;
-        while (getline(file, line) ) {
-            lines.push_back(line);
+    namespace utils {
+        vector<string> get_lines(string filename) {
+            vector<string> lines;
+            ifstream file(filename);
+            string line;
+            while (getline(file, line) ) {
+                lines.push_back(line);
+            }
+            return lines;
         }
-        return lines;
-    }
 
-    vector<string> split(const string &s, char delim, bool empties) {
-        vector<string> elems;
-        split(s, delim, elems, empties);
-        return elems;
-    }
-
-    vector<string> &split(const string &s, char delim, vector<string> &elems, bool empties) {
-        stringstream ss(s);
-        string item;
-        while (getline(ss, item, delim)) {
-            if (!empties && item == "")
-                continue;
-            elems.push_back(item);
+        vector<string> split(const string &s, char delim, bool empties) {
+            vector<string> elems;
+            split(s, delim, elems, empties);
+            return elems;
         }
-        return elems;
-    }
 
-    bool startof(const string &prefix, const string &str) {
-        return prefix.size() <= str.size() && str.compare(0, prefix.size(), prefix) == 0;
-    }
-
-    bool startofany(const string &prefix, const vector<string> &strs) {
-        for (auto const &str : strs) {
-            if (startof(prefix, str))
-                return true;
+        vector<string> &split(const string &s, char delim, vector<string> &elems, bool empties) {
+            stringstream ss(s);
+            string item;
+            while (getline(ss, item, delim)) {
+                if (!empties && item == "")
+                    continue;
+                elems.push_back(item);
+            }
+            return elems;
         }
-        return false;
+
+        bool startof(const string &prefix, const string &str) {
+            return prefix.size() <= str.size() && str.compare(0, prefix.size(), prefix) == 0;
+        }
+
+        bool startofany(const string &prefix, const vector<string> &strs) {
+            for (auto const &str : strs) {
+                if (startof(prefix, str))
+                    return true;
+            }
+            return false;
+        }
     }
 }
