e9e335610e430a3094e9dc54ff67daab8f43ab38
Benjamin Esquivel <benjaesq@me.com>
2014-05-28 23:25:18 -0500

    including common makefile rules and definitions

---
+++ mk/common-defs.mk
@@ -0,0 +1,9 @@
+#benjaesq@me.com
+
+INSTALL = install
+INSTALL_x = $(INSTALL) -m 755
+INSTALL_d = $(INSTALL) -d
+INSTALL_f = $(INSTALL) -m 644
+
+DESTDIR := $(ROOTDIR)/destdir
+bindir = /usr/bin
--- mk/common-rules.mk
+++ mk/common-rules.mk
@@ -1,1 +1,6 @@
 # benjaesq@me.com
+
+DESTDIRS=$(DESTDIR)$(bindir)
+
+$(DESTDIRS):
+	$(INSTALL_d) $@
