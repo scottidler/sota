7273806fb081c870736cf34938244b732cb6dad6
scottidler <scottidler@gmail.com>
2014-06-12 23:47:55 -0700

    added the hehe file... -sai

---
+++ src/hehe.h
@@ -0,0 +1,10 @@
+#ifndef __SOTA_HEHE__
+#define __SOTA_HEHE__ = 1
+
+//useless file motivated by an idea from my friend Benjamin Esquivel
+namespace sota {
+    class SotaHehe {
+    }
+}
+
+#endif /* __SOTA_HEHE__ */
