5c19aeaf208ab3c35c786dea86ae309dd1076e6b
Scott A. Idler <scott.a.idler@intel.com>
2014-06-25 15:15:27 -0700

    deleted test code... -sai

--- src/sota.cpp
+++ src/sota.cpp
@@ -1,6 +1,5 @@
 // sota.cpp : Defines the entry point for the console application.
 //
-#include <boost/regex.hpp>
 #include <array>
 #include <string>
 #include <iostream>
@@ -16,19 +15,10 @@ using namespace sota;
 int main(int argc, char* argv[]) {
 
     std::string source = "1 + 2";
-    boost::regex re("[0-9]+");
-    boost::match_results<std::string::const_iterator> matches;
-    boost::regex_match(source, matches, re);
-    std::cout << matches[1].str() << std::endl;
-    return 0;
 
     auto parser = Parser(Type2Symbol);
     parser.Source = source;
     auto token = parser.Scan();
-
-    std::cout
-        << token.Index << std::endl
-        << token.Length << std::endl;
 
     return 0;
 }
