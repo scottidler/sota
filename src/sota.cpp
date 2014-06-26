eefe0a1f647e1285a09f8c7d8efbf7d9ed2f48bb
Scott A. Idler <scott.a.idler@intel.com>
2014-06-25 18:15:24 -0700

    fixed lingering case issue, Source -> source ... -sai

--- src/sota.cpp
+++ src/sota.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
     std::string source = "1 + 2";
 
     auto parser = Parser(Type2Symbol);
-    parser.Source = source;
+    parser.source = source;
     auto token = parser.LookAhead(5);
     std::cout << token << std::endl;
 
