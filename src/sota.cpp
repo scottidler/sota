2c8130b11795542f43f4e35063e9c8c6021e44bf
scottidler <scottidler@gmail.com>
2014-06-28 09:07:03 -0700

    testing individual functions... -sai

--- src/sota.cpp
+++ src/sota.cpp
@@ -14,11 +14,20 @@ using namespace sota;
 
 int main(int argc, char* argv[]) {
 
-    std::string source = "a + b";
+    std::string source = "a * b + c";
 
     auto parser = Parser(Type2Symbol);
-    auto *ast = parser.Parse(source);
-    std::cout << "Parse result: " << ast->Print() << std::endl;
+    parser.source = source;
+    auto t1 = parser.Scan();
+    std::cout << "expect a; t1=" << t1 << std::endl;
+    auto t2 = parser.Consume();
+    std::cout << "expect a; t2=" << t2 << std::endl;
+    auto t3 = parser.LookAhead(1);
+    std::cout << "expect *; t3=" << t3 << std::endl;
+    auto t4 = parser.Consume();
+    std::cout << "expect *; t4=" << t4 << std::endl;
+    //auto *ast = parser.Parse(source);
+    //std::cout << "Parse result: " << ast->Print() << std::endl;
 
     return 0;
 }
