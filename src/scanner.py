9871a8aa75dcbb68fd0f37e99405b1a066db3de4
scottidler <scottidler@gmail.com>
2014-07-29 12:54:04 -0700

    updated scanner to show skipping algorithm... -sai

--- src/scanner.py
+++ src/scanner.py
@@ -5,14 +5,14 @@ import re
 import sys
 
 symbols = [
-    ('eof', '\0', 0),
-    ('ws', '[ \t]+', 0),
-    ('id', '([0-9]+)?[A-Za-z]+([0-9]+)?', 0),
-    ('num', '[0-9]+', 0),
-    ('add', '\+', 50),
-    ('sub', '-', 50),
-    ('mul', '\*', 60),
-    ('div', '\/', 60),
+    ('eof', '\0', 0, False),
+    ('ws', '[ \t]+', 0, True),
+    ('id', '([0-9]+)?[A-Za-z]+([0-9]+)?', 0, False),
+    ('num', '[0-9]+', 0, False),
+    ('add', '\+', 50, False),
+    ('sub', '-', 50, False),
+    ('mul', '\*', 60, False),
+    ('div', '\/', 60, False),
 ]
 
 class Scanner(object):
@@ -27,40 +27,45 @@ class Scanner(object):
         token = symbols[0] + (self.position, 0, '\0')
         if self.position < len(self.source):
             end = self.position
-            for tipo, pattern, lbp in symbols:
+            for tipo, pattern, lbp, skip in symbols:
                 text = self.source[self.position:]
                 string = self.source[self.position:]
                 m = re.match(pattern, string)
                 match = m.group(0) if m else ''
                 length = len(match) if match else 0
                 if self.position + length > end or (token != None and lbp > token[2] and self.position + length == end):
-                    token = (tipo, pattern, lbp, self.position, length, self.source[self.position:self.position+length])
+                    token = (tipo, pattern, lbp, skip, self.position, length, self.source[self.position:self.position+length])
                     end = self.position + length
         return token
 
-    def LookAhead(self, distance):
-        if distance == 0:
-            return tokens[self.index]
-        while distance >= len(self.tokens) - self.index:
-            token = self.Scan()
-            self.tokens += token,
-            self.position += token[4]
+    def LookAhead(self, distance, skips=False):
+        token = None
+        i = self.index + 1
+        while distance:
+            if i < len(self.tokens):
+                token = self.tokens[i]
+            else:
+                token = self.Scan()
+                self.position += token[5]
+                self.tokens += token,
+            skip = token[3]
+            if skips or token[3] == False: #if skip
+                distance -= 1
+            i += 1
+        return token
 
-        curr = self.index + distance
-        return self.tokens[curr]
-
-    def Consume(self, expected = None, message = None):
-        token = self.LookAhead(1)
+    def Consume(self, expected=None, message=None, skips=False):
+        token = self.LookAhead(1, skips=skips)
         self.index += 1
         if expected and token[0] != expected:
             raise Exception(message or '')
         return token
 
-    def Tokenize(self):
-        token = self.Consume()
+    def Tokenize(self, skips=False):
+        token = self.Consume(skips=skips)
         while token[0] != 'eof':
-            token = self.Consume()
-        return self.tokens
+            token = self.Consume(skips=skips)
+        return [ token for token in self.tokens if not skips and not token[3] ]
 
 def main(args):
     scanner = Scanner(' '.join(args) )
