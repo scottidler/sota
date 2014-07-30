399f39a114eb9dc63ca9f8f0d8028e2ef823474a
scottidler <scottidler@gmail.com>
2014-07-29 18:08:30 -0700

    fixed exceptions to take __FILE__ and __LINE__ values... -sai

--- src/exceptions.h
+++ src/exceptions.h
@@ -2,25 +2,37 @@
 #define __SOTA_EXCEPTIONS__ = 1
 
 #include <cstring>
+#include <sstream>
 #include <iostream>
 #include <exception>
 
 namespace sota {
     class SotaException: public std::exception {
-        std::string _message;
+        const char *_filename;
+        size_t _line;
+        const std::string _message;
+        std::string _what;
     public:
-        SotaException(std::string message) {
-            _message = message;
+        SotaException(const char *filename, size_t line, const std::string message = "")
+            : _filename(filename)
+            , _line(line)
+            , _message(message) {
+            std::ostringstream out;
+            out << "filename=" << _filename
+                << " line=" << _line
+                << " msg=" << _message
+                << std::endl;
+            _what = out.str();
         }
         virtual const char * what() const throw() {
-            return _message.c_str();
+            return _what.c_str();
         }
     };
 
     class SotaNotImplemented: SotaException {
     public:
-        SotaNotImplemented(std::string message)
-            : SotaException(message) {
+        SotaNotImplemented(const char *filename, size_t line, const std::string message = "")
+            : SotaException(filename, line, message) {
         }
     };
 }
