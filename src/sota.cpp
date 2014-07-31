9d4f767d8617f6ff4483da98939a7a6b136c4b2e
scottidler <scottidler@gmail.com>
2014-07-31 12:32:57 -0700

    implemented sota interface via newly added tclap header files ... -sai

--- src/sota.cpp
+++ src/sota.cpp
@@ -12,52 +12,39 @@
 #include "parser.h"
 #include "bindpower.h"
 
+#include "tclap/CmdLine.h"
+
 using namespace sota;
 
-void usage() {
-    std::cout << "usage" << std::endl;
-}
+int main(int argc, char **argv) {
+    try {
+        TCLAP::CmdLine cmdline("Sota Description", ' ', "0.1");
+        TCLAP::SwitchArg tokenize("t", "tokenize", "Tokenize the code", cmdline, false);
+        TCLAP::ValueArg<std::string> code("c", "code", "Code to evaluate", true, "", "string");
+        TCLAP::UnlabeledValueArg<std::string> file("file", "Sota file with main definined", true, "",  "file");
+        cmdline.xorAdd(code, file);
 
-std::vector<std::string> accumulate(int i, int argc, char *argv[]) {
-    std::vector<std::string> items;
-    for(; i < argc; ++i) {
-        items.push_back(argv[i]);
+        cmdline.parse(argc, argv);
+
+        std::string source = code.getValue();
+        std::string filename = file.getValue();
+
+        auto parser = SotaParser();
+
+        if (!filename.empty()) {
+            auto ast = parser.ParseFile(filename);
+            std::cout << filename << ":" << std::endl;
+            std::cout << ast->Print() << std::endl;
+        }
+        else if (!source.empty()) {
+            auto ast = parser.Parse(source);
+            std::cout << source << ":" << std::endl;
+            std::cout << ast->Print() << std::endl;
+        }
     }
-    return items;
-}
-
-int main(int argc, char* argv[]) {
-
-    std::vector<std::string> filenames;
-    std::vector<std::string> sources;
-    switch (argc) {
-        case 1:
-            usage();
-            return 0;
-        default:
-            if (std::string(argv[1]) == "-c") {
-                sources = accumulate(2, argc, argv);
-            }
-            else {
-                filenames = accumulate(1, argc, argv);
-            }
-            break;
-    }
-
-    auto parser = SotaParser();
-
-    for (auto source : sources) {
-        std::cout << "sources:" << std::endl;
-        auto *ast = parser.Parse(source);
-        std::cout << source << ":" << std::endl;
-        std::cout << ast->Print() << std::endl;
-    }
-
-    for (auto filename : filenames) {
-        std::cout << "filenames:" << std::endl;
-        auto *ast = parser.ParseFile(filename);
-        std::cout << filename << ":" << std::endl;
-        std::cout << ast->Print() << std::endl;
+    catch (TCLAP::ArgException &e) {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+        return 1;
     }
     return 0;
 }
